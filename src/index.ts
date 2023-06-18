851b699b8ec18834c35bd64151a70700c9ac6bf6
m-51 <m51@m51.pl>
2023-06-18 17:49:14 +0200

    feat: export `globby`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
+import { globby } from 'globby';
 import { dev } from './dev.js';
 import { build } from './build.js';
 
-export { dev, build, fs };
+export { dev, build, fs, globby };
